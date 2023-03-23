453098bc4edc7f8fda42328995fb5216e748d5fe
kiran68 <kiranurshil88@gmail.com>
2023-03-23 17:50:58 +0530

    Add: Ensure The The Wining Position

---
+++ WinningPosition.java
@@ -0,0 +1,37 @@
+package com.bridgelabz.snakeladderprogram;
+
+public class WinningPosition {
+	public static void main(String[] args) {
+		int position = 0;
+
+		while (position < 100) {
+			int diceRoll = (int) Math.floor(Math.random() * 6) + 1;
+			int input = (int) Math.floor(Math.random() * 3);
+
+			if (input == 0) {
+				System.out.println("No Play PlayerStays at " + position);
+
+			} else if (input == 1) {
+				position += diceRoll;
+
+				if (position > 100) {
+					position -= diceRoll;
+
+				}
+
+				else if (position == 100) {
+					break;
+				}
+				System.out.println("Ladder.position moves to " + position);
+			} else {
+				// position -= diceRoll;
+				position = position - diceRoll;
+				if (position < 0) {
+					position = 0;
+				}
+				System.out.println("Snake .player moves to " + position);
+			}
+		}
+		System.out.println("Congratulation ..you have to won");
+	}
+}
